165ea0f423ef17946f63aa0632bb8eba8c3532ce
kerringtonwells <kwells2@live.com>
2017-03-23 23:59:43 -0400

    Update replace.go

--- replace.go
+++ replace.go
@@ -1,4 +1,15 @@
-
-     var replacer = strings.NewReplacer(" ", "\", \"")
-     str := "ls -lhtr"
-     str = replacer.Replace(str)
+ package main
+ 
+ import (
+     "fmt"
+     "strings"
+     "os/exec"
+ )
+ 
+ 
+ 
+ func main() {
+  var replacer = strings.NewReplacer(" ", "\", \"")
+  str := "ls -lhtr"
+  str = replacer.Replace(str)
+ }

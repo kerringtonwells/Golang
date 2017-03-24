7c0662cb6af2522a506dffc3114a19d9cd2da01f
kerringtonwells <kwells2@live.com>
2017-03-24 00:18:16 -0400

    Update inspect_variable.go

--- inspect_variable.go
+++ inspect_variable.go
@@ -3,5 +3,7 @@ import "fmt"
 
 func main() {
   x := "Hello World\n"
-  fmt.Printf("%v", x)
+  fmt.Printf("%v", x) // Print variables real value
+  fmt.Printf("%T", x) // Print variables type
+  
 }
